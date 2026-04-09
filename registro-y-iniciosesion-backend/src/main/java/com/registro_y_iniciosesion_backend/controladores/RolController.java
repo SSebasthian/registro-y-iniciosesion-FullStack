1fbf67b1e9600ab3fb21953dbb35bd7adc2dad60
SSEBASTHIAN <ssebasthian.perez@gmail.com>
2026-04-09 10:31:31 -0500

    SE CREA CONTROLADOR (ROL) Y SE PRUEBA EN POSTMAN

---
+++ registro-y-iniciosesion-backend/src/main/java/com/registro_y_iniciosesion_backend/controladores/RolController.java
@@ -0,0 +1,98 @@
+package com.registro_y_iniciosesion_backend.controladores;
+
+
+import com.registro_y_iniciosesion_backend.entidades.Permisos;
+import com.registro_y_iniciosesion_backend.entidades.Rol;
+import com.registro_y_iniciosesion_backend.servicios.PermisosService;
+import com.registro_y_iniciosesion_backend.servicios.RolService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("roles")
+public class RolController {
+
+
+    private final RolService rolService;
+    private final PermisosService permisosService;
+
+    // Inyección del servicio mediante constructor
+    public RolController(RolService rolService,  PermisosService permisosService) {
+        this.rolService = rolService;
+        this.permisosService = permisosService;
+    }
+
+    // Crear un rol (POST /roles)
+    @PostMapping
+    public Rol crearRol(@RequestBody Rol rol) {
+        return rolService.crear(rol);
+    }
+
+    // Listar todos los permisos (GET /permisos)
+    @GetMapping
+    public List<Rol> listarRoles() {
+        return rolService.listar();
+    }
+
+    // Buscar un permiso por ID (GET /permisos/{id})
+    @GetMapping("/{id}")
+    public Rol buscarPorId(@PathVariable Long id) {
+        return rolService.buscarPorId(id);
+    }
+
+    // Agregar un permiso a un rol
+    @PostMapping("/{idRol}/agregar-permiso/{idPermiso}")
+    public Rol agregarPermiso(
+            @PathVariable Long idRol,           // ID del rol
+            @PathVariable Long idPermiso) {     // ID del permiso
+
+        // Buscar el rol y permiso en BD
+        Rol rol = rolService.buscarPorId(idRol);
+        Permisos permiso = permisosService.buscarPorId(idPermiso);
+
+        // Validar que existan
+        if (rol == null || permiso == null) {
+            return null; // puedes retornar un ResponseEntity más elegante si quieres
+        }
+
+        // Agregar el permiso al rol y guardar
+        return rolService.agregarPermiso(rol, permiso);
+    }
+}
+
+
+
+/////////////////////////////////////////////////////////////////////////////////
+////////////////////////// COMO PROBAR EN POSTMAN  //////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+
+
+// 1 . Crear Un Rol
+//     Metodo: POST
+//     URL: http://localhost:8080/roles
+//     Body -> raw -> JSON
+
+//        {
+//            "nombre": "Administrador"
+//        }
+
+
+// 2. Listar Todos Los Roles
+//    Metodo: GET
+//     URL: http://localhost:8080/roles
+
+
+// 3. Buscar Un Rol Por ID
+//    Metodo: GET
+//    URL: http://localhost:8080/roles/1
+
+
+
+// 4. Agregar Permiso a Un ROl
+//    Metodo: POST
+//    URL: http://localhost:8080/roles/1/agregar-permiso/1
+
+
+/////////////////////////////////////////////////////////////////////////////////
+
