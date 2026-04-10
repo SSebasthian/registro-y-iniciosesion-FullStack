d2f349eaf7912f41586d47c435eb633762edf154
SSEBASTHIAN <ssebasthian.perez@gmail.com>
2026-04-10 06:58:18 -0500

    Se Diseña Autenticacion (ACCESO)

--- registro-y-iniciosesion-frontend/src/app/pagina/autenticacion/acceso/acceso.component.ts
+++ registro-y-iniciosesion-frontend/src/app/pagina/autenticacion/acceso/acceso.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon'
+
 
 @Component({
   selector: 'app-acceso',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './acceso.component.html',
   styleUrl: './acceso.component.css'
 })
