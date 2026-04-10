87f0336495e0eeca70dc9858605237ab9658825d
SSEBASTHIAN <ssebasthian.perez@gmail.com>
2026-04-10 06:12:30 -0500

    Se Enruta Pagina de (REGISTRO)

--- registro-y-iniciosesion-frontend/src/app/app.routes.ts
+++ registro-y-iniciosesion-frontend/src/app/app.routes.ts
@@ -1,14 +1,23 @@
 import { Routes } from '@angular/router';
 import { AccesoComponent } from './pagina/autenticacion/acceso/acceso.component';
+import { RegistroComponent } from './pagina/autenticacion/registro/registro.component';
 
 
 export const routes: Routes = [
     {
         path: '',
-        component: AccesoComponent
+        component: RegistroComponent
     },
     {
-        path: 'acceso',
-        component: AccesoComponent
+        path: 'autenticacion',
+        children:[
+                {path: 'acceso',
+                component:AccesoComponent
+            },
+                {path: 'registro',
+                component:RegistroComponent
+            }
+        ]
     }
+    
 ];
