f336e5303ae200c76152422027136300e23269aa
SSEBASTHIAN <ssebasthian.perez@gmail.com>
2026-04-10 07:03:23 -0500

    Se ENRUTAN Entre si (REGISTRO-ACCESO)

--- registro-y-iniciosesion-frontend/src/app/pagina/autenticacion/acceso/acceso.component.ts
+++ registro-y-iniciosesion-frontend/src/app/pagina/autenticacion/acceso/acceso.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon'
+import { RouterLink, RouterModule } from "@angular/router";
 
 
 @Component({
   selector: 'app-acceso',
-  imports: [MatIconModule],
+  imports: [MatIconModule, RouterLink, RouterModule],
   templateUrl: './acceso.component.html',
   styleUrl: './acceso.component.css'
 })
--- registro-y-iniciosesion-frontend/src/app/pagina/autenticacion/registro/registro.component.ts
+++ registro-y-iniciosesion-frontend/src/app/pagina/autenticacion/registro/registro.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon'
+import { RouterLink, RouterModule } from "@angular/router";
+
 
 
 
 @Component({
   selector: 'app-registro',
-  imports: [MatIconModule],
+  imports: [MatIconModule, RouterLink,RouterModule],
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.css'
 })
