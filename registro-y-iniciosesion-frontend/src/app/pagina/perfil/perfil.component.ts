07e5d8de99443a0f73f5ab90f8a3d1a242849485
SSEBASTHIAN <ssebasthian.perez@gmail.com>
2026-04-11 07:56:41 -0500

    Se Diseña Pagina (PERFIL) FRONT-END

--- registro-y-iniciosesion-frontend/src/app/pagina/perfil/perfil.component.ts
+++ registro-y-iniciosesion-frontend/src/app/pagina/perfil/perfil.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon'
+
 
 @Component({
   selector: 'app-perfil',
-  imports: [],
+  imports: [MatIconModule ],
   templateUrl: './perfil.component.html',
   styleUrl: './perfil.component.css'
 })
